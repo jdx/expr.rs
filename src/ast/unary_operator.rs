63f0cbe6bfce366c0397b8b39315d42536ced084
Risu <79110363+risu729@users.noreply.github.com>
2025-06-16 08:39:33 +1000

    fix: parse signs as unary operators (#17)

--- src/ast/unary_operator.rs
+++ src/ast/unary_operator.rs
@@ -1,5 +1,4 @@
 use crate::ast::node::Node;
-use crate::ast::unary_operator::UnaryOperator::Not;
 use crate::Rule;
 use crate::Value::Bool;
 use crate::{bail, Result};
@@ -12,13 +11,17 @@ use std::str::FromStr;
 pub enum UnaryOperator {
     #[strum(serialize = "!")]
     Not,
+    #[strum(serialize = "+")]
+    Positive,
+    #[strum(serialize = "-")]
+    Negative,
 }
 
 impl From<Pair<'_, Rule>> for UnaryOperator {
     fn from(pair: Pair<Rule>) -> Self {
         trace!("[unary_operator] {pair:?}");
         match pair.as_str() {
-            "not" => Not,
+            "not" => UnaryOperator::Not,
             op => UnaryOperator::from_str(op)
                 .unwrap_or_else(|_| unreachable!("Invalid operator {op}")),
         }
@@ -34,9 +37,19 @@ impl Environment<'_> {
     ) -> Result<Value> {
         let node = self.eval_expr(ctx, node)?;
         let result = match operator {
-            Not => match node {
+            UnaryOperator::Not => match node {
                 Bool(b) => Bool(!b),
                 _ => bail!("Invalid operand for operator !"),
+            },
+            UnaryOperator::Positive => match node {
+                Value::Number(n) => Value::Number(n),
+                Value::Float(f) => Value::Float(f),
+                _ => bail!("Invalid operand for operator +"),
+            },
+            UnaryOperator::Negative => match node {
+                Value::Number(n) => Value::Number(-n),
+                Value::Float(f) => Value::Float(-f),
+                _ => bail!("Invalid operand for operator -"),
             },
         };
 
